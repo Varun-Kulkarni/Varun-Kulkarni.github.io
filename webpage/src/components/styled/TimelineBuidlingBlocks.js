ee0abacb31ed41b374f3fcf7eeffaa5423c1bddc
Varun Kulkarni <vskred126@gmail.com>
2022-05-21 11:36:53 -0400

    Update to timeline

--- webpage/src/components/styled/TimelineBuidlingBlocks.js
+++ webpage/src/components/styled/TimelineBuidlingBlocks.js
@@ -2,12 +2,17 @@ import React from "react";
 import styled from 'styled-components';
 import { MainText, StyledH2, StyledH3, StyledH3Mod } from "../styled/comps";
 import { device, colors } from "../../constants";
+import timeline_bg from '../../assets/timeline_bg.svg'
 
 export const Timeline = styled.div`
     @media ${device.md} {
         display: flex;
         flex-direction: column;
         margin: 2rem 0;
+        background: url('${timeline_bg}');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-attachment: fixed;
 
     }
 `
@@ -16,11 +21,31 @@ export const TimelineCard = styled.div`
     position: relative;
     margin: 2rem 0;
     border: 2px solid ${colors.white};
+    z-index: 1;
     @media ${device.md} {
         margin: 1rem 0;
-        width: 50%;
+        width: 47.5%;
         &:nth-child(odd) {
             align-self: end;
+        }
+        &:after {
+            content: ' ';
+            background-color: #fff;
+            position: absolute;
+            right: -1.5rem;
+            top: calc(50% - 1.5rem);
+            width: 0;
+            height: 0;
+            border-top: 1.5rem solid transparent;
+            border-bottom: 1.5rem solid transparent;
+            border-left: 1.5rem solid ${colors.white};
+            background: transparent;
+        }
+        &:nth-child(odd)::after {
+            border-right: 1.5rem solid ${colors.white};
+            border-left: 1.5rem transparent;
+            right: 0;
+            left: -1.5rem;
         }
     }
 `
